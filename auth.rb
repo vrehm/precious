9e7bfb999f637fe46929b65881e4e19876020ed5
Vincent REHM <vincentrehm@yahoo.fr>
2016-11-08 20:18:27 +0100

    config vars

--- auth.rb
+++ auth.rb
@@ -4,6 +4,9 @@ module Precious
     auth_config_path = "secret.yml"
     if File.exists?(auth_config_path)
       AUTH_CONFIG = YAML.load_file(auth_config_path)
+    else
+      AUTH_CONFIG['user'] = ENV['user']
+      AUTH_CONFIG['pass'] = ENV['pass']
     end
 
     use Rack::Auth::Basic, "Restricted Area" do |username, password|
